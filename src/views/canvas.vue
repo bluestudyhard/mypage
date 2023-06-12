9a0d0dc2dd2bfb77af193ec9e6557a6a77632f76
bluestudyhard <3164752596@qq.com>
2023-06-12 10:06:12 +0800

    1

--- src/views/canvas.vue
+++ src/views/canvas.vue
@@ -5,20 +5,7 @@
 </template>
 <script setup>
 import { ref, reactive, watch, computed, onMounted } from "vue";
-// const canvasRef = ref(null);
-// const ctx = ref(null);
 
-// const initCanvas = () => {
-//   const canvas = canvasRef.value;
-//   ctx.value = canvas.getContext("2d");
-//   ctx.fillRect(0, 0, 1000, 1000);
-//   ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-//   ctx.strokeRect(0, 0, 1, 1);
-//   ctx.beginPath();
-// };
-// onMounted(() => {
-//   initCanvas();
-// });
 const canvasRef = ref();
 const scale = ref(1.0);
 
@@ -43,27 +30,6 @@ function drawRectangle() {
     scale.value * 100
   );
   ctx.stroke();
-}
-
-// 监听鼠标滚轮事件，实现缩放
-function handleMouseWheel(e) {
-  const delta = e.deltaY || e.detail || e.wheelDelta;
-  console.log(delta);
-  if (delta < 0) {
-    // 放大
-    scale.value += 0.1;
-  } else {
-    // 缩小
-    scale.value -= 0.1;
-  }
-  if (scale.value < 0.1) {
-    scale.value = 0.1;
-  }
-  if (scale.value > 3.0) {
-    scale.value = 3.0;
-  }
-  //   drawCircle();
-  e.preventDefault(); // 这里禁止冒泡的原因是，会触发原本页面的滚动事件
 }
 
 const scrollEvent = (e) => {

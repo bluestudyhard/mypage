fcd1735dcc10082dff43eecca13b611d219b65f7
bluestudyhard <3164752596@qq.com>
2023-09-07 10:18:27 +0800

    fix:更改了一些样式和删除了一些注释，为了更好的merge

--- src/router/index.js
+++ src/router/index.js
@@ -16,22 +16,7 @@ function importViews() {
   }
 }
 importViews();
-// const routes = [
-//   {
-//     path: '/',
-//     component: () => import('@/views/home.vue'),
-//     meta: {
-//       keepAlive: true // 设置该页面需要缓存
-//     }
-//   },
-//   {
-//     path: '/canvas',
-//     component: () => import('@/views/canvas.vue'),
-//     meta: {
-//       keepAlive: true // 设置该页面需要缓存
-//     }
-//   }
-// ];
+
 
 const router = createRouter({
   history: createWebHashHistory(),
